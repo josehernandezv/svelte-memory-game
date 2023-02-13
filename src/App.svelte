18b81a364ea5012e03a54647d3831dc44e046a42
josehernandezv <jose.andres.art@gmail.com>
2023-02-12 18:51:27 -0600

    Lesson 1

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import toadSrc from "./assets/toad.png";
+  import flowerSrc from "./assets/flower.png";
+  import tenCoinSrc from "./assets/10coins.png";
+  import twentyCoinSrc from "./assets/20coins.png";
+  import starSrc from "./assets/star.png";
+  import oneUpSrc from "./assets/1up.png";
+  import Card from "./lib/Card.svelte";
+
+  let cardImages = [
+    toadSrc,
+    flowerSrc,
+    tenCoinSrc,
+    twentyCoinSrc,
+    starSrc,
+    oneUpSrc,
+  ];
+  let cards = [...cardImages, ...cardImages, ...cardImages];
+</script>
+
+<main>
+  {#each cards as card}
+    <Card src={card} />
+  {/each}
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-gap: 1em;
+  }
+</style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,25 @@
+<script>
+  import coverSrc from "../assets/cover.png";
+
+  export let src;
+</script>
+
+<div class="card">
+  <img src={coverSrc} alt="Carta" />
+</div>
+
+<!-- <img {src} alt="Carta" /> -->
+<style>
+  .card {
+    position: relative;
+    border: 10px solid transparent;
+  }
+  .card:hover {
+    border-image: url(../assets/cardBorder.png) 30 round;
+    overflow: hidden;
+    cursor: pointer;
+  }
+  .card img {
+    filter: drop-shadow(12px 12px 0 #200060);
+  }
+</style>
